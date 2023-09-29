ea7e4c6a966d254bee3646a9e7e4292ae23fbf46
AshwinSathian <ashwinsathyan19@gmail.com>
2023-09-29 23:52:52 +0530

    Added @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +12,7 @@ import { KnobModule } from 'primeng/knob';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, CalculatorComponent],
@@ -26,6 +27,12 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
     ChartModule,
     ProgressSpinnerModule,
     KnobModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],

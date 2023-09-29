b01d39a2430f3c2d66d7de8fe9aaf4af46c8d9fe
AshwinSathian <ashwinsathyan19@gmail.com>
2023-09-30 00:59:45 +0530

    Added PWA assets

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/ngsw-worker.js');
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
